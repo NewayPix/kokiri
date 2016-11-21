e69c8984b111e2447151b94e1f2c657ce45011ad
Rafael Campos Nunes <rafaelnunes@engineer.com>
2016-11-21 18:52:19 -0200

    Add OpenGL information
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 
 
 int main(int argc, char **argv) {
-	
+
 	bool running = true;
 
 	SDL_Window* window = nullptr;
@@ -37,14 +37,19 @@ int main(int argc, char **argv) {
 		Debug::log_err("Reason: ", SDL_GetError());
 		return 1;
 	}
-	
+
 	SDL_GLContext gl = SDL_GL_CreateContext(window);
-	// Send the gl context to the renderer 
+	// Send the gl context to the renderer
 	Renderer opengl_renderer = Renderer(gl);
 
 	uint32_t frame = 0;
 
 	uint32_t fps = 60;
+
+	Debug::log("OpenGL version: ", glGetString(GL_VERSION));
+	Debug::log("OpenGL vendor: ", glGetString(GL_VENDOR));
+	Debug::log("OpenGL renderer: ", glGetString(GL_RENDERER));
+	Debug::log("GLSL version: ", glGetString(GL_SHADING_LANGUAGE_VERSION));
 
 	while (running) {
 		frame = SDL_GetTicks();
@@ -56,6 +61,9 @@ int main(int argc, char **argv) {
                 running = false;
                 break;
             case SDL_KEYDOWN:
+				/**
+				 * This is synchronous state.
+				 */
             	switch(event.key.keysym.sym) {
             		case SDLK_x:
             			opengl_renderer.rotate(4, 1, 0, 0);
@@ -71,7 +79,7 @@ int main(int argc, char **argv) {
                 //std::cout << "Event: " << event.type << std::endl;
             	break;
 		}
-
+		opengl_renderer.rotate(1, 1, 1, 0);
         opengl_renderer.render_cube(2.0);
         SDL_GL_SwapWindow(window);
 
