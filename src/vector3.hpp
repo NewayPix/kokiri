1694c76b3c6dcff51d3b2fe9f92cc8bfd9b1295a
Rafael Campos Nunes <rafaelnunes@engineer.com>
2017-11-06 02:46:09 -0200

    Fix indentation and reshape classes
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/vector3.hpp
+++ src/vector3.hpp
@@ -1,3 +1,4 @@
+
 /**
  * MIT License
  *
@@ -31,32 +32,31 @@
 template <class T>
 class Vector3 {
 public:
-	T m_x;
-	T m_y;
-	T m_z;
+    T m_x;
+    T m_y;
+    T m_z;
 public:
-	Vector3(T x, T y, T z) {
-		m_x = x;
-		m_y = y;
-		m_z = z;
-	}
-
-	~Vector3() {}
-
-	Vector3<T> norm() {
-		Vector3<T> normalized = *this;
-
-		normalized.m_x /= mag();
-		normalized.m_y /= mag();
-		normalized.m_z /= mag();
-
-		return normalized;
-	}
-
-	T mag() {
-		return  sqrt(pow(m_x, 2) + pow(m_y, 2) + pow(m_z, 2));
-	}
+    Vector3(T x, T y, T z) {
+	m_x = x;
+	m_y = y;
+	m_z = z;
+    }
+    
+    ~Vector3() {}
+    
+    Vector3<T> norm() {
+	Vector3<T> normalized = *this;
+	
+	normalized.m_x /= mag();
+	normalized.m_y /= mag();
+	normalized.m_z /= mag();
+	
+	return normalized;
+    }
+    
+    T mag() {
+	return  sqrt(pow(m_x, 2) + pow(m_y, 2) + pow(m_z, 2));
+    }
 };
-
 
 #endif // VECTOR_H
