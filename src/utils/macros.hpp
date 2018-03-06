b7d00b0b090f9997df4cac8f239c084d1ee72e95
Rafael Campos Nunes <rafaelnunes@engineer.com>
2018-03-05 22:53:17 -0300

    Now there are real ansi-colors
        The compiler was shouting about the old macros definitions, so
    changing to ansi-colors fixed the problem.
    
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/utils/macros.hpp
+++ src/utils/macros.hpp
@@ -2,12 +2,15 @@
 #define MACROS_H
 
 #if __unix__
-#define WARNING_COLOR "\e[1;33m"
-#define ERROR_COLOR "\e[0;31m"
-#define SUCCESS_COLOR ""
+#define WARNING_COLOR "\x1b[33;1m"
+#define ERROR_COLOR   "\x1b[31;1m"
+#define SUCCESS_COLOR "\x1b[34;1m"
+#define RESET_COLOR   "\x1b[0m"
 #else
 #define WARNING_COLOR ""
 #define ERROR_COLOR ""
-#endif
+#define SUCCESS_COLOR ""
+#define RESET_COLOR ""
+#endif // __unix__
 
 #endif // MACROS_H
