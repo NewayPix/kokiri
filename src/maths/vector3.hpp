e02241d4663ea95b7b5433734baac16288dcefa7
Rafael Campos Nunes <rafaelnunes@engineer.com>
2018-08-29 11:49:29 -0300

    Move the maths to a folder that will be a module
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/maths/vector3.hpp
+++ src/maths/vector3.hpp
@@ -1,4 +1,3 @@
-
 /**
  * MIT License
  *
@@ -28,7 +27,7 @@
 #define VECTOR_H
 
 #include <cmath>
-// TODO: Limit the instantiation types to numbers only.
+
 template <class T>
 class Vector3 {
 public:
@@ -37,25 +36,25 @@ public:
     T m_z;
 public:
     Vector3(T x, T y, T z) {
-	m_x = x;
-	m_y = y;
-	m_z = z;
+        m_x = x;
+        m_y = y;
+        m_z = z;
     }
-    
+
     ~Vector3() {}
-    
+
     Vector3<T> norm() {
-	Vector3<T> normalized = *this;
-	
-	normalized.m_x /= mag();
-	normalized.m_y /= mag();
-	normalized.m_z /= mag();
-	
-	return normalized;
+        Vector3<T> normalized = *this;
+
+        normalized.m_x /= mag();
+        normalized.m_y /= mag();
+        normalized.m_z /= mag();
+
+        return normalized;
     }
-    
+
     T mag() {
-	return  sqrt(pow(m_x, 2) + pow(m_y, 2) + pow(m_z, 2));
+        return  sqrt(pow(m_x, 2) + pow(m_y, 2) + pow(m_z, 2));
     }
 };
 
