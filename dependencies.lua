7f390e1f395ee173d1ee0f078c94563a6ca7f902
Rafael Campos Nunes <rafaelnunes@enginer.com>
2023-04-01 20:01:59 -0300

    Modify compilation files
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ dependencies.lua
@@ -0,0 +1,10 @@
+
+-- Kokiri dependencies
+
+IncludeDir = {}
+
+IncludeDir["box2d"] = "%{wks.location}/kokiri/vendor/box2d/include"
+IncludeDir["glad"] = "%{wks.location}/kokiri/vendor/glad/include"
+IncludeDir["glm"] = "%{wks.location}/kokiri/vendor/glm"
+
+LibraryDir = {}
