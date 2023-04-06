2731b16298fbe8685220378020de5cb0bb0e07d7
Rafael Campos Nunes <rafaelnunes@engineer.com>
2023-04-06 02:55:09 -0300

    Fix segmentation fault
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- kokiri-test/application.cpp
+++ kokiri-test/application.cpp
@@ -17,53 +17,58 @@ int main(int argc, char *argv[]) {
 
     SDL_Event event;
 
-    Window window(800, 600, "Kokiri Framework", SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
+    // create a scope so the window and the renderer can call their destructor
+    // after the scope ends and the call to quit is successfully made.
+    {
+        Window window(800, 600, "Kokiri Framework", SDL_WINDOW_OPENGL | SDL_WINDOW_SHOWN);
 
-    auto renderer = Renderer2D(std::move(window), Renderer2D::Version::OPENGL_4_6);
+        auto renderer = Renderer2D(std::move(window), Renderer2D::Version::OPENGL_4_6);
 
-    renderer.information();
+        renderer.information();
 
-    while (is_running) {
-        SDL_PollEvent(&event);
+        while (is_running) {
+            SDL_PollEvent(&event);
 
-        switch (event.type) {
-        case SDL_QUIT:
-            is_running = false;
-            break;
-        case SDL_KEYDOWN:
-            switch(event.key.keysym.sym) {
-            case SDLK_ESCAPE:
+            switch (event.type) {
+            case SDL_QUIT:
                 is_running = false;
                 break;
-            case SDLK_d:
-                debug = !debug;
-                break;
-            case SDLK_f:
-                {
-                    is_fullscreen = !is_fullscreen;
+            case SDL_KEYDOWN:
+                switch(event.key.keysym.sym) {
+                case SDLK_ESCAPE:
+                case SDLK_q:
+                    is_running = false;
+                    break;
+                case SDLK_d:
+                    debug = !debug;
+                    break;
+                case SDLK_f:
+                    {
+                        is_fullscreen = !is_fullscreen;
 
-                    int state = 0;
+                        int state = 0;
 
-                    if (is_fullscreen) {
-                        state = SDL_SetWindowFullscreen(window.get_window(), SDL_WINDOW_FULLSCREEN);
-                    } else {
-                        state = SDL_SetWindowFullscreen(window.get_window(), 0);
+                        if (is_fullscreen) {
+                            state = SDL_SetWindowFullscreen(window.get_window(), SDL_WINDOW_FULLSCREEN);
+                        } else {
+                            state = SDL_SetWindowFullscreen(window.get_window(), 0);
+                        }
+
+                        if (state < 0) {
+                            Log::error("failed to toggle fullscreen, reason ", SDL_GetError());
+                        }
                     }
-
-                    if (state < 0) {
-                        Log::error("failed to toggle fullscreen, reason ", SDL_GetError());
-                    }
+                    break;
+                default:
+    #ifdef DEBUG
+                    Log::info("event: ", event.type);
+    #endif // DEBUG
+                    break;
                 }
-                break;
-            default:
-#ifdef DEBUG
-                Log::info("event: ", event.type);
-#endif // DEBUG
-                break;
             }
-        }
 
-        SDL_GL_SwapWindow(window.get_window());
+            SDL_GL_SwapWindow(window.get_window());
+        }
     }
 
     SDL_Quit();
