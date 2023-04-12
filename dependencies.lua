6c8a9fa0a9f7ae574f762894e1e2dc1b7db1cc9c
Rafael Campos Nunes <rafaelnunes@engineer.com>
2023-04-12 14:13:34 -0300

    Add kokiri itself as a folder for the test projects
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- dependencies.lua
+++ dependencies.lua
@@ -8,6 +8,13 @@ IncludeDir["glad"] = "%{wks.location}/kokiri/vendor/glad/include"
 IncludeDir["fmt"] = "%{wks.location}/kokiri/vendor/fmt/include"
 IncludeDir["glm"] = "%{wks.location}/kokiri/vendor/glm"
 
+-- The kokiri include folder for the tests are not quite clear from their
+-- perspective so I need to do the following.
+
+IncludeDir["kokiri"] = "%{wks.location}/kokiri"
+
+--
+
 LibraryDir = {}
 
 LibraryDir["kokiri"] = "%{wks.location}/build/kokiri/"
