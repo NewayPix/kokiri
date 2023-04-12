3f36306a3065bd0f5da51c4408313d9a9cb16f10
Rafael Campos Nunes <rafaelnunes@engineer.com>
2023-04-12 11:44:57 -0300

    Update the dependencies file
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- dependencies.lua
+++ dependencies.lua
@@ -5,9 +5,11 @@ IncludeDir = {}
 
 IncludeDir["box2d"] = "%{wks.location}/kokiri/vendor/box2d/include"
 IncludeDir["glad"] = "%{wks.location}/kokiri/vendor/glad/include"
+IncludeDir["fmt"] = "%{wks.location}/kokiri/vendor/fmt/include"
 IncludeDir["glm"] = "%{wks.location}/kokiri/vendor/glm"
 
 LibraryDir = {}
 
 LibraryDir["kokiri"] = "%{wks.location}/build/kokiri/"
 LibraryDir["glad"] = "%{wks.location}/kokiri/vendor/glad/build/glad/"
+LibraryDir["box2d"] = "%{wks.location}/kokiri/vendor/box2d/build/"
