39da566ee3eff5a8f449d1f1ee65cdf8e5247eb3
Rafael Campos Nunes <rafaelnunes@engineer.com>
2023-04-16 16:49:10 -0300

    Update application class
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- kokiri/src/core/application.hpp
+++ kokiri/src/core/application.hpp
@@ -1,7 +1,27 @@
 #pragma once
 
+#include "core/game.hpp"
+#include "core/window.hpp"
+#include "core/references.hpp"
+#include "graphics/opengl/renderer2d.hpp"
+#include "graphics/sdl/renderer2d.hpp"
+
 namespace Kokiri {
     class Application {
+    private:
+        SharedRef<Window> m_window;
 
+        SharedRef<Graphics::OpenGL::Renderer2D> m_opengl_renderer;
+        SharedRef<Graphics::SDL::Renderer2D> m_sdl_renderer;
+
+        ScopedRef<Game> m_game;
+    public:
+        enum class ApplicationRenderType {
+            SDL,
+            OpenGL,
+        };
+
+        Application(ApplicationRenderType render_type);
+        ~Application();
     };
 }
