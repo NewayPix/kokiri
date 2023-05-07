26dfe51065bc342bd39ee4b5f7f70827579fac96
Rafael Campos Nunes <rafaelnunes@engineer.com>
2023-05-07 00:17:39 -0300

    Add a types class
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ kokiri/src/core/types.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <cstdint>
+
+#include "math/vector2.hpp"
+#include "math/vector3.hpp"
+
+namespace Kokiri {
+    template <typename T>
+    using v2 = Math::Vector2<T>;
+
+    template <typename T>
+    using v3 = Math::Vector3<T>;
+
+    using u8 = uint8_t;
+    using u16 = uint16_t;
+    using u32 = uint32_t;
+
+    using i8 = int8_t;
+    using i16 = int16_t;
+    using i32 = int32_t;
+}
