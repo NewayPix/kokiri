6923b71bc1d903b2bd9194711fa02b1e58a60b02
Rafael Campos Nunes <rafaelnunes@engineer.com>
2023-05-15 00:38:18 -0300

    Update event class implementation
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- kokiri/src/core/event.cpp
+++ kokiri/src/core/event.cpp
@@ -1,30 +1,47 @@
 #include "kkr.hpp"
 #include "event.hpp"
 
-
 namespace Kokiri {
+    Event::Event() {
+        m_key_state = SDL_GetKeyboardState(nullptr);
+    }
 
-    Event::Event() {}
     Event::~Event() {}
 
     void Event::pool() {
+        clear();
+
         while (SDL_PollEvent(&m_event)) {
             switch(m_event.type) {
             case SDL_MOUSEMOTION:
-
+                m_mouse_move = true;
+                m_mouse_motion_event = m_event.motion;
+                break;
+            case SDL_MOUSEBUTTONDOWN:
+            case SDL_MOUSEBUTTONUP:
+                m_mouse_click = true;
+                m_mouse_button_event = m_event.button;
                 break;
             case SDL_KEYDOWN:
+                m_key_pressed = true;
                 break;
             }
         }
+    }
+
+    void Event::clear() {
+        m_mouse_click = false;
+        m_mouse_move = false;
+
+        m_key_pressed = false;
     }
 
     bool Event::is_key_down(Key key) {
         return false;
     }
 
-    bool Event::is_mouse_click(Mouse mouse) {
-        return false;
+    bool Event::is_mouse_click(Mouse::Buttons button) {
+        return m_mouse_click && m_mouse_button_event.button == button;
     }
 
     v2<int> Event::get_mouse_position() {
--- kokiri/src/core/event.hpp
+++ kokiri/src/core/event.hpp
@@ -7,19 +7,28 @@
 
 namespace Kokiri {
     class Event {
-        static SDL_Event m_event;
-        static SDL_MouseMotionEvent m_mouse_motion_event;
-        static SDL_MouseButtonEvent m_mouse_button_event;
+    private:
+        SDL_Event m_event;
+        SDL_MouseMotionEvent m_mouse_motion_event;
+        SDL_MouseButtonEvent m_mouse_button_event;
 
-        static u8 *m_key_state;
+        u8 *m_key_state;
+
+        bool m_mouse_click;
+        bool m_mouse_move;
+
+        bool m_key_pressed;
     public:
         Event();
         ~Event();
 
-        enum class Mouse {
-            LeftButton,
-            MiddleButton,
-            RightButton,
+        class Mouse {
+        public:
+            enum Buttons {
+                LeftButton = 1,
+                MiddleButton = 2,
+                RightButton = 3,
+            };
         };
 
         enum class Key {
@@ -36,11 +45,23 @@ namespace Kokiri {
          * @brief Pool events from SDL
          *
          */
-        static void pool();
+        void pool();
 
-        static bool is_key_down(Key key);
-        static bool is_mouse_click(Mouse mouse);
+        /**
+         * @brief Clear any registered event
+         *
+         */
+        void clear();
 
-        static v2<int> get_mouse_position();
+        bool is_key_down(Key key);
+        bool is_mouse_click(Mouse::Buttons mouse);
+
+        /**
+         * @brief Get the mouse position
+         *
+         * @return v2<int> A two dimensional vector containing the x and y
+         * coordinates of the mouse on the window
+         */
+        v2<int> get_mouse_position();
     };
 }
