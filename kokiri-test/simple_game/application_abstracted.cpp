edbb4b9bcf5cd63cb86bbd92424e709156beaa42
Rafael Campos Nunes <rafaelnunes@engineer.com>
2023-05-15 01:49:48 -0300

    Move logic to test functions
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- kokiri-test/simple_game/application_abstracted.cpp
+++ kokiri-test/simple_game/application_abstracted.cpp
@@ -6,7 +6,16 @@
 #include "core/sound/track.hpp"
 #include "graphics/sdl/sprite.hpp"
 
+void test_1();
+void test_2();
+
 int main(int argc, char *argv[]) {
+    test_2();
+
+    return 0;
+}
+
+void test_1() {
     using namespace Kokiri;
     using namespace Kokiri::Graphics::SDL;
 
@@ -25,7 +34,26 @@ int main(int argc, char *argv[]) {
     bgm.play(1);
 
     game.loop();
+}
 
-    return 0;
+void test_2() {
+    using namespace Kokiri;
+    using namespace Kokiri::Graphics::SDL;
+
+    Game game("A Game", 1024, 600);
+
+    Track bgm("stageState.ogg");
+    Track effect("boom.wav");
+
+    Sprite background(game.get_window(), "ocean.jpg");
+
+    game.bind(FunctionType::Render, [&game, &background](){
+        background.render(0, 0);
+    });
+
+    effect.play(1);
+    bgm.play(1);
+
+    game.loop();
 }
 
