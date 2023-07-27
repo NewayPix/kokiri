ba741808ca4d752b68705acf352888dcbe4712cc
Rafael Campos Nunes <rafaelnunes@engineer.com>
2023-07-27 10:48:14 -0300

    Improves implementation of the tilemap
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- kokiri/src/graphics/sdl/tilemap.hpp
+++ kokiri/src/graphics/sdl/tilemap.hpp
@@ -6,25 +6,37 @@
 #include "graphics/sdl/tileset.hpp"
 
 #include <string>
+#include <vector>
 
 namespace Kokiri {
     namespace Graphics {
         namespace SDL {
             class Tilemap : public Component<ComponentType> {
             private:
+                SharedRef<Window> m_window;
 
+                std::string m_file;
+
+                Tileset* m_tileset;
+
+                // a collection of tiles given its position on the layer (z-axis)
+                std::vector<std::vector<i32>> m_tiles;
+
+                i32 m_rows;
+                i32 m_columns; // x * columns + y = index
+                i32 m_layers;
             public:
-                Tilemap(const std::string& file, Tileset* tile_set);
+                Tilemap(SharedRef<Window> window, const std::string& file, Tileset* tileset);
                 ~Tilemap();
 
-                u32 at(v3<u32> position);
+                i32 at(v3<i32> position);
 
                 void render() override;
-                void render_layer();
+                void render_layer(i32 layer, v2<i32> position);
 
                 u32 get_width();
                 u32 get_height();
-                u32 get_depth();
+                i32 get_depth();
             };
         }
     }
